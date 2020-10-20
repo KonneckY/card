49ada2b00710f69b49558b6051f659618f85459d
KubaNocon7890 <42960635+KonneckY@users.noreply.github.com>
2020-10-20 20:20:55 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const element = document.querySelector(".bird-img");
 
+const cardB = document.querySelector(".cardB");
+const btn_close = document.querySelector(".zamknij");
+const btn_open = document.querySelector(".btn");
+
+btn_open.addEventListener('click', function(){
+    cardB.style.display = "grid";
+});
+btn_close.addEventListener('click', function(){
+    cardB.style.display = "none";
+});
+
 setInterval(() => {
-    element.style.setProperty("--ml", Math.random() * 300 + "px");
+    element.style.setProperty("--ml", Math.random() * 200 + "px");
 }, 300)
